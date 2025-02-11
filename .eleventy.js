a7df7f0b5dacd94305f88cadd33f939534073312
Godsent <85384933+godsentsalvaloza@users.noreply.github.com>
2025-02-11 15:30:16 +0800

    fixed backend blockage

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./source/styles");
   eleventyConfig.addPassthroughCopy("./source/scripts/out");
   eleventyConfig.addPassthroughCopy("./source/resources/main");
+  eleventyConfig.addPassthroughCopy("./source/admin");
 
   return {
     dir: {
